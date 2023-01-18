47d48a35ff74c3c519d02bfa9e27c9a522729b57
rianllauo <rianllauo@gmail.com>
2023-01-18 22:17:02 +0700

    databse config

--- server/pkg/mysql/mysql.go
+++ server/pkg/mysql/mysql.go
@@ -11,7 +11,7 @@ var DB *gorm.DB
 
 func DatabaseInit() {
 	var err error
-	dsn := "root:@tcp(containers-us-west-199.railway.app:7519)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:VhyUH0UaMDt9F2pjENLg@tcp(containers-us-west-199.railway.app:7519)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	// mysql://root:VhyUH0UaMDt9F2pjENLg@containers-us-west-199.railway.app:7519/railway
 	if err != nil {
